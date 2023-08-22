15d6c5f20ebe3e1cfd5e7c9f447644f24bcaa6b9
ebrahim20254 <ebrahimkhalil125800@gmail.com>
2023-08-22 19:15:37 +0600

    assignment done

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
-function handleCardClick(target){
-    const selectedCardContainer  = document.getElementById('selected-card');
-    const cardName = target.parentNode.children[5].innerText;
-    const li = document.createElement('li');
-    li.innerText = cardName;
-    selectedCardContainer.appendChild(li);
+
+let totalPrice = 0;
+let count = 0;
+function handleCardClick(target) {
+    const priceText = target.querySelector('h5').innerText;
+    const price = Number(priceText.split(" ")[0]);
+    totalPrice += price;
+    count += 1;
+
+    document.getElementById("total").innerText = `Total Price: ${totalPrice}.00TK`;
+
+    const cardName = target.querySelector('h3').innerText;
+
+    // Create a new card element for the selected card
+    const newCard = document.createElement('div');
+    newCard.classList.add('selected-card');
+    newCard.innerText = `${count} ${cardName}`;
+
+    // Append the new card to the container
+    const selectedCardContainer = document.getElementById('selected-card');
+    selectedCardContainer.appendChild(newCard);
 }
+
+// Update the initial display of total price
+document.getElementById("total").innerText = `Total Price: ${totalPrice.toFixed(2)} TK`;
+
+console.log(totalPrice);
+
+function applyBtn() {
+    const discountPercentage = 0.20; // 20% discount
+    const discountedAmount = totalPrice * discountPercentage;
+    const discountedTotal = totalPrice - discountedAmount;
+
+    document.getElementById("discounted-total").innerText = `Discounted Total: ${discountedTotal.toFixed(2)} TK`;
+
+    document.getElementById("all-total").innerText = `Total: ${discountedTotal.toFixed(2)} TK`;
+    if(total >= 200){
+        applyBtn.disabled = true;
+    }else{
+        applyBtn.disabled = false;
+    }
+}
+
+
+document.getElementById('purchase-btn').addEventListener('click', function(){
+    const totalPrice = discountedTotal('all-total');
+    console.log(totalPrice);
+
+    if(totalPrice > '0'){
+        const purchaseBtn = document.getElementById('purchase-btn');
+        purchaseBtn.removeAttribute('disabled');
+        console.log('button enabled');
+    }else{
+        console.log('button remains disabled');
+    }
+});
+
+
+
